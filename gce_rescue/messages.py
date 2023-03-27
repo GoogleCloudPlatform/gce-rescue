1f354077b198470ab57c2dd063667da639cb8a5a
soulless-viewer <29832584+soulless-viewer@users.noreply.github.com>
2023-03-27 17:48:53 +0200

    chore: reformat gce_rescue/messages.py

--- gce_rescue/messages.py
+++ gce_rescue/messages.py
@@ -12,23 +12,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-""" List of messages to inform and educate the user. """
+"""List of messages to inform and educate the user."""
 
 from gce_rescue.rescue import Instance
 
+
 def tip_connect_ssh(vm: Instance) -> str:
-  return (f'└── Your instance is READY! You can now connect your instance '
-    f' {vm.name} via:\n  1. CLI. (add --tunnel-through-iap if necessary)\n'
-    f'    $ gcloud compute ssh {vm.name} --zone={vm.zone} '
-    f'--project={vm.project} --ssh-flag="-o StrictHostKeyChecking=no"\n  OR\n'
-    f'  2. Google Cloud Console:\n'
-    f'    https://ssh.cloud.google.com/v2/ssh/projects/{vm.project}/zones/'
-    f'{vm.zone}/instances/{vm.name}?authuser=0&hl=en_US&useAdminProxy=true&'
-    f'troubleshoot4005Enabled=true\n')
+  return (
+      '└── Your instance is READY! You can now connect your instance '
+      f' {vm.name} via:\n  1. CLI. (add --tunnel-through-iap if necessary)\n'
+      f'    $ gcloud compute ssh {vm.name} --zone={vm.zone} '
+      f'--project={vm.project} --ssh-flag="-o StrictHostKeyChecking=no"\n  OR\n'
+      '  2. Google Cloud Console:\n'
+      f'    https://ssh.cloud.google.com/v2/ssh/projects/{vm.project}/zones/'
+      f'{vm.zone}/instances/{vm.name}?authuser=0&hl=en_US&useAdminProxy=true&'
+      'troubleshoot4005Enabled=true\n'
+  )
+
 
 def tip_restore_disk(vm: Instance) -> str:
-  return (f'└── The instance {vm.name} was restored! Use the snapshot below '
-    f'if you need to restore the modification made while the instance was '
-    f'in rescue mode.\n Snapshot name: {vm.disks["disk_name"]}-{vm.ts}\n'
-    f' More information: '
-    f'https://cloud.google.com/compute/docs/disks/restore-snapshot\n')
+  return (
+      f'└── The instance {vm.name} was restored! Use the snapshot below '
+      'if you need to restore the modification made while the instance was '
+      f'in rescue mode.\n Snapshot name: {vm.disks["disk_name"]}-{vm.ts}\n'
+      ' More information: '
+      'https://cloud.google.com/compute/docs/disks/restore-snapshot\n'
+  )
