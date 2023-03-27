85a1ab85990f2f9102650b7be4d9b0d224203624
soulless-viewer <29832584+soulless-viewer@users.noreply.github.com>
2023-03-27 17:48:54 +0200

    chore: reformat gce_rescue/tasks/disks_test.py

--- gce_rescue/tasks/disks_test.py
+++ gce_rescue/tasks/disks_test.py
@@ -20,68 +20,60 @@ from absl import logging
 from gce_rescue.rescue import Instance
 from gce_rescue.tasks import disks
 from gce_rescue.test.mocks import (
-  mock_api_object,
-  MOCK_TEST_VM,
-  MOCK_TEST_DATA,
+    mock_api_object,
+    MOCK_TEST_VM,
+    MOCK_TEST_DATA,
 )
 
 logging.set_verbosity('INFO')
 # logging.set_verbosity('DEBUG')
 
+
 class DisksTest(absltest.TestCase):
+
   def setUp(self):
     self.vm = Instance(test_mode=True, **MOCK_TEST_VM)
-    self.instance_data =  self.vm.data
+    self.instance_data = self.vm.data
 
   def test_list_disk(self):
     self.vm.compute = mock_api_object(['disks'])
     disk_filter = f'labels.rescue={self.vm.ts}'
-    result = disks.list_disk(self.vm,
-      self.vm.project_data,
-      disk_filter)
+    result = disks.list_disk(self.vm, self.vm.project_data, disk_filter)
     self.assertTrue(len(result) > 0)
     self.assertEqual(result[0]['name'], MOCK_TEST_DATA['disk'])
 
-
   def test_attach_disk(self):
     self.vm.compute = mock_api_object([
-      'disks',
-      'disks',
-      'operations',
-      'operations',
+        'disks',
+        'disks',
+        'operations',
+        'operations',
     ])
     disk_filter = f'labels.rescue={self.vm.ts}'
-    disk_list = disks.list_disk(self.vm,
-      self.vm.project_data,
-      disk_filter)
+    disk_list = disks.list_disk(self.vm, self.vm.project_data, disk_filter)
     disk_name = disk_list[0]['name']
     disks_ = self.instance_data['disks']
     disk = disks_[0]
     device_name = disk['deviceName']
-    disks.attach_disk(
-       self.vm,
-       disk_name,
-       device_name)
-
+    disks.attach_disk(self.vm, disk_name, device_name)
 
   def test_config_rescue_disks(self):
     self.vm.compute = mock_api_object([
-      'operations',
-      'operations',
-      'operations',
-      'operations',
-      'operations',
-      'disks',
+        'operations',
+        'operations',
+        'operations',
+        'operations',
+        'operations',
+        'disks',
     ])
     disks.config_rescue_disks(self.vm)
 
-
   def test_restore_original_disk(self):
     self.vm.compute = mock_api_object([
-      'operations',
-      'operations',
-      'operations',
-      'operations',
+        'operations',
+        'operations',
+        'operations',
+        'operations',
     ])
     disks.restore_original_disk(self.vm)
 
