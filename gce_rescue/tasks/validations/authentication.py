7d2e760633f841a1b632f897e54400f4f5e16ffe
soulless-viewer <29832584+soulless-viewer@users.noreply.github.com>
2023-03-27 17:48:54 +0200

    chore: reformat gce_rescue/tasks/validations/authentication.py

--- gce_rescue/tasks/validations/authentication.py
+++ gce_rescue/tasks/validations/authentication.py
@@ -12,13 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-""" Authentication validation to be called from ../pre_validations.py """
+"""Authentication validation to be called from ../pre_validations.py"""
 import googleapiclient.discovery
 import google.auth
 import sys
 from gce_rescue.test.mocks import mock_api_object
 
 PROJECT = ''
+
 
 def _get_auth():
   global PROJECT
@@ -36,11 +37,9 @@ def _get_auth():
     print(msg, file=sys.stderr)
     sys.exit(1)
 
+
 def authenticate_check(
-  zone: str,
-  instance_name: str,
-  project: str = None,
-  test_mode: bool = False
+    zone: str, instance_name: str, project: str = None, test_mode: bool = False
 ) -> googleapiclient.discovery.Resource:
   global PROJECT
   PROJECT = project
@@ -51,14 +50,11 @@ def authenticate_check(
   if not credentials:
     return False
   service = googleapiclient.discovery.build(
-    'compute',
-    'v1',
-    credentials = credentials
+      'compute', 'v1', credentials=credentials
   )
   request = service.instances().get(
-		project = PROJECT,
-		zone = zone,
-		instance = instance_name)
+      project=PROJECT, zone=zone, instance=instance_name
+  )
   try:
     request.execute()
     return service
@@ -67,26 +63,30 @@ def authenticate_check(
     print(msg, file=sys.stderr)
     sys.exit(1)
 
+
 def project_name() -> str:
   return PROJECT
 
+
 def _info_auth_refresh() -> str:
   return (
-	'    Please use application-default Credentials (ADC) to authenticate:\n'
-	'    $ gcloud auth login --update-adc'
+      '    Please use application-default Credentials (ADC) to authenticate:\n'
+      '    $ gcloud auth login --update-adc'
   )
+
 
 def _info_auth_cred() -> str:
   return (
-	'    Please use application-default Credentions (ADC) to authenticate:\n'
-	'     $ gcloud auth application-default login\n'
-	f'     $ gcloud auth application-default set-quota-project {PROJECT}'
+      '    Please use application-default Credentions (ADC) to authenticate:\n'
+      '     $ gcloud auth application-default login\n'
+      f'     $ gcloud auth application-default set-quota-project {PROJECT}'
   )
+
 
 def _info_no_project() -> str:
   return (
-	'    Was not possible to find the project where the VM is created.\n'
-	'    You can use the option --project to declare the project-id or '
-  'set to your configuration:\n'
-	'    $ gcloud config set project PROJECT_ID'
+      '    Was not possible to find the project where the VM is created.\n'
+      '    You can use the option --project to declare the project-id or '
+      'set to your configuration:\n'
+      '    $ gcloud config set project PROJECT_ID'
   )
