0ff35e49d2edf7b8dedde993df1561aec93190f9
soulless-viewer <29832584+soulless-viewer@users.noreply.github.com>
2024-02-22 14:04:33 +0100

    style: syntax factoring in pre_validations.py

--- gce_rescue/tasks/pre_validations.py
+++ gce_rescue/tasks/pre_validations.py
@@ -12,45 +12,48 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-""" Validations that should be performed before continue the script execution.
-    Each validations steps can be appended to the class Validations() and keep
-    the file under validations/ folder."""
+"""Validations that should be performed before continue the script
+execution. Each validations steps can be appended to the class
+Validations() and keep the file under validations/ folder.
+"""
+
+import googleapiclient.discovery
 
 from dataclasses import dataclass, field
-import googleapiclient.discovery
 from gce_rescue.tasks.validations.authorization import authorize_check
-from gce_rescue.tasks.validations.authentication import (
-  authenticate_check,
-  project_name
-)
+from gce_rescue.tasks.validations.authentication import (authenticate_check,
+    project_name)
+
 
 @dataclass
 class Validations:
-  """Run different validations before continue the script.
-  	Return:
-  		discovery.build object.
-  """
-  zone: str
-  name: str
-  project: str = None
-  test_mode: bool = field(default_factory=False)
+    """Run different validations before continue the script.
 
-  def _authentication(self):
-    return authenticate_check(
-      project = self.project,
-      zone = self.zone,
-      instance_name = self.name,
-      test_mode = self.test_mode,
-    )
+    Return:
+        discovery.build object.
+    """
 
-  def __post_init__(self):
-    if not self.test_mode:
-      authorize_check(project = self.project)
+    zone: str
+    name: str
+    project: str = None
+    test_mode: bool = field(default_factory=False)
 
-  @property
-  def compute(self) -> googleapiclient.discovery.Resource:
-    return self._authentication()
+    def _authentication(self):
+        return authenticate_check(
+            project=self.project,
+            zone=self.zone,
+            instance_name=self.name,
+            test_mode=self.test_mode,
+        )
 
-  @property
-  def adc_project(self) -> str:
-    return project_name()
+    def __post_init__(self):
+        if not self.test_mode:
+            authorize_check(project=self.project)
+
+    @property
+    def compute(self) -> googleapiclient.discovery.Resource:
+        return self._authentication()
+
+    @property
+    def adc_project(self) -> str:
+        return project_name()
